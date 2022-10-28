39eac45ba370f77ff3b473ae739354e340460f97
Zemburych <116352585+Zemburych@users.noreply.github.com>
2022-10-28 09:18:49 +0300

    решили задачу 6

--- Les03/Program.cs
+++ Les03/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+Console.WriteLine("Ввелите число 1  ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 % 2==1)
+            {
+                Console.WriteLine("число нечетное");
+           
+            }
+            else
+            {
+                Console.WriteLine("Число четное");
+            }
