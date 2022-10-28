0b0b49ec742ab42531ef67f866aaeaf3ec275d5e
Zemburych <116352585+Zemburych@users.noreply.github.com>
2022-10-28 09:25:18 +0300

    Создали задача 8

---
+++ Les4/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+Console.WriteLine("Ввелите число 1  ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 % 2==1)
+            {
+                Console.WriteLine("число нечетное");
+           
+            }
+            else
+            {
+                Console.WriteLine("Число четное");
+            }
