bd846931cfa0680612dad5cf9ec2001f26d1c53c
Zemburych <116352585+Zemburych@users.noreply.github.com>
2022-10-28 08:29:47 +0300

    добавили блок схему

--- les01/Program.cs
+++ les01/Program.cs
@@ -1,2 +1,16 @@
-﻿
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+Console.WriteLine("Ввелите число 1  ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Ввелите число 2 ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+if (number1 > number2)
+{
+    Console.WriteLine($"большее - {number1}");
+    Console.WriteLine($"меньшее - {number2}");
+
+}
+else
+{
+    Console.WriteLine($"большее - {number2}");
+    Console.WriteLine($"меньшее - {number1}");
+}
