620816bd564ee87650df7f2233b8715e56c80aa1
Zemburych <116352585+Zemburych@users.noreply.github.com>
2022-10-28 08:57:09 +0300

    решили вторую задачу

--- Les02/Program.cs
+++ Les02/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+Console.WriteLine("Ввелите число 1  ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Ввелите число 2 ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Ввелите число 2 ");
+int number3 = Convert.ToInt32(Console.ReadLine());
+if (number1 > number2)
+{   
+    if (number1 > number3)
+    {
+        Console.WriteLine($"большее - {number1}");
+    }
+    else
+    {
+        Console.WriteLine($"большее - {number3}");
+    }
+}
+else
+{
+    if (number2 > number3)
+    {
+        Console.WriteLine($"большее - {number2}");
+    }
+    else
+    {
+        Console.WriteLine($"большее - {number3}");
+    }
+}
